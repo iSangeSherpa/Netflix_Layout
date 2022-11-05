3e2067aca38fbf119000b7996b5b4bfc5942fd77
Sange Sherpa <sherpasange1215@gmail.com>
2022-11-05 15:31:04 +0545

    Initial Commit

---
+++ Netflix/Controllers/DownloadsViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ Netflix/Controllers/UpcomingViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground    }
+}
